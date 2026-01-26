63a946cb91a9df1ad1fdf36096b44c44b11ad013
Kevin <146332692+AnikiDev@users.noreply.github.com>
2026-01-26 12:27:45 +0100

    documentation de get_next_id
    Add function to generate unique identifiers

--- pixel_tracer/src/id.c
+++ pixel_tracer/src/id.c
@@ -1,7 +1,19 @@
 #include "id.h"
 
+unsigned long long int get_next_id();
+
 static unsigned long long int global_id = 0;
 
+/**
+ * @brief Génère et retourne un nouvel identifiant unique.
+ *
+ * Cette fonction incrémente l'identifiant global interne et retourne
+ * sa nouvelle valeur. Elle permet de garantir l'unicité des identifiants
+ * générés au cours de l'exécution du programme.
+ *
+ * @return unsigned long long int
+ *         Le nouvel identifiant généré.
+ */
 unsigned long long int get_next_id() {
     global_id++;
     return global_id;

cec670ff51b9d8c952e3babce0d6cd38cb13e0d6
Kevin <146332692+AnikiDev@users.noreply.github.com>
2026-01-28 13:47:40 +0100

    documentation pour le reste de focntions
    Added functions to set, save, and load the global ID.

--- pixel_tracer/src/id.c
+++ pixel_tracer/src/id.c
@@ -1,6 +1,9 @@
 #include "id.h"
 
 unsigned long long int get_next_id();
+void set_id(unsigned long long int id);
+void save_id();
+void load_id();
 
 static unsigned long long int global_id = 0;
 
@@ -19,14 +22,41 @@ unsigned long long int get_next_id() {
     return global_id;
 }
 
+/**
+ * @brief Définit manuellement la valeur de l'identifiant global.
+ *
+ * Cette fonction permet d'initialiser ou de modifier l'identifiant global
+ * utilisé pour la génération des prochains identifiants.
+ *
+ * @param id unsigned long long int
+ *        Nouvelle valeur à assigner à l'identifiant global.
+ */
 void set_id(unsigned long long int id) {
     global_id = id;
 }
 
+/**
+ * @brief Sauvegarde l'identifiant global courant.
+ *
+ * Cette fonction est destinée à enregistrer la valeur actuelle de
+ * l'identifiant global dans un fichier (par exemple ID_FILE) afin de
+ * permettre sa restauration lors d'une prochaine exécution du programme.
+ *
+ * @note Fonction non implémentée pour le moment.
+ */
 void save_id() {
     // todo save id in file ID_FILE
 }
 
+/**
+ * @brief Charge l'identifiant global depuis une source persistante.
+ *
+ * Cette fonction est destinée à charger la valeur de l'identifiant global
+ * depuis un fichier (par exemple ID_FILE) afin de reprendre la génération
+ * des identifiants sans collision.
+ *
+ * @note Fonction non implémentée pour le moment.
+ */
 void load_id() {
     // todo load id from file ID_FILE
 }

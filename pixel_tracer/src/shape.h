113bdb95853f837f73b732734619109d162b01fc
Kevin <146332692+AnikiDev@users.noreply.github.com>
2026-01-28 14:40:32 +0100

    documentation pour shape.h

--- pixel_tracer/src/shape.h
+++ pixel_tracer/src/shape.h
@@ -1,30 +1,52 @@
 #ifndef _SHAPE_H_
-#  define _SHAPE_H_
+#define _SHAPE_H_
+
+/**
+ * @file shape.h
+ * @brief Gestion des formes géométriques pour Pixel Tracer.
+ *
+ * Ce module fournit des types de données et des fonctions pour créer,
+ * manipuler et supprimer des formes géométriques telles que Point, Line,
+ * Square, Rectangle, Circle, Polygon et Curve.
+ *
+ * Structures principales :
+ *  - Point, Line, Squar, Rectangle, Cercle, Polygon, Curve
+ *  - Shape : encapsule une forme avec ID, couleur, rotation et remplissage
+ *
+ * Fonctions exportées :
+ *  - Création et destruction de formes individuelles
+ *  - Création de Shape "exportable" pour l'API
+ *  - Fonctions de sérialisation des formes (sprint_*)
+ */
 
 typedef enum shape_type { POINT, LINE, SQUAR, RECTANGLE, CERCLE,
     POLYGON, CURVE
 } Shape_type;
+
 typedef enum color { BLACK, WIGHT, RED, GREEN } Color;
 
+/** @struct Point */
 struct point {
     int pos_x;
     int pos_y;
 };
 typedef struct point Point;
 
+/** @struct Line */
 struct line {
     Point *p1;
     Point *p2;
 };
 typedef struct line Line;
 
+/** @struct Squar */
 struct squar {
     Point *p1;
     int length;
 };
 typedef struct squar Squar;
 
-
+/** @struct Rectangle */
 struct rectangle {
     Point *p1;
     int width;
@@ -32,20 +54,21 @@ struct rectangle {
 };
 typedef struct rectangle Rectangle;
 
+/** @struct Cercle */
 struct cercle {
     Point *center;
     int radus;
 };
 typedef struct cercle Cercle;
 
-
+/** @struct Polygon */
 struct polygon {
     int n;
     Point **points;
 };
 typedef struct polygon Polygon;
 
-
+/** @struct Curve */
 struct curve {
     Point *p1;
     Point *p2;
@@ -54,12 +77,13 @@ struct curve {
 };
 typedef struct curve Curve;
 
-
-
+/** @struct Shape
+ *  Structure générique pour représenter une forme avec ID, type, couleur, rotation, etc.
+ */
 struct shape {
     unsigned long long int id;
     Shape_type shape_type;
-    void *ptrShape;
+    void *ptrShape;  /**< pointeur vers la forme spécifique */
     unsigned char fill;
     float thickness;
     Color color;
@@ -68,6 +92,7 @@ struct shape {
 typedef struct shape Shape;
 
 
+/** Création et destruction des formes de base */
 Point *create_point(int px, int py);
 void delete_point(Point * point);
 Line *create_line(Point * p1, Point * p2);
@@ -83,8 +108,10 @@ void delete_polygon(Polygon * polygon);
 Curve *create_curve(Point * p1, Point * p2, Point * p3, Point * p4);
 void delete_curve(Curve * curve);
 
+/** Création d'une Shape vide */
 Shape *create_empty_shape(Shape_type shape_type);
 
+/** Sérialisation des formes en chaîne de caractères */
 void sprint_point(Point * p, char *str);
 void sprint_line(Line * line, char *str);
 void sprint_squar(Squar * squar, char *str);
@@ -94,23 +121,20 @@ void sprint_polygon(Polygon * polygon, char *str);
 void sprint_curve(Curve * curve, char *str);
 
 
-/**
- * Exporte only thoses functions 
- **/
+/** Fonctions exportées pour créer des Shapes directement */
 Shape *create_point_shape(int px, int py);
 Shape *create_line_shape(int px1, int py1, int px2, int py2);
 Shape *create_square_shape(int px, int py, int length);
 Shape *create_rectangle_shape(int px, int py, int width, int height);
 Shape *create_cercle_shape(int px, int py, int radus);
-//Shape *create_polygon_shape(int n, ...);
 Shape *create_polygon_shape(int n, int *tab);
-
 Shape *create_curve_shape(int px1, int py1, int px2, int py2, int px3,
                           int py3, int px4, int py4);
 
+/** Supprime une Shape et toutes ses formes internes */
 void delete_shape(Shape * shape);
+
+/** Sérialisation d'une Shape en chaîne de caractères */
 void sprint_shape(Shape * shape, char *str);
-
-
 
 #endif
